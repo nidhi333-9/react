289c117d59b8d9f1e51e6d96b04ae9acf86012f9
Nidhi <84095928+nidhi333-9@users.noreply.github.com>
2022-09-21 21:52:37 +0530

    Update App.jsx

--- project-two/business-card/src/App.jsx
+++ project-two/business-card/src/App.jsx
@@ -5,7 +5,6 @@ import Main from './components/Main'
 function App() {
   return (
     <div className="App">
-      <h1 className='text-xl font-bold text-green-900'>Hello React + Tailwind With Vite</h1>
       <Main />
     </div>
   )
