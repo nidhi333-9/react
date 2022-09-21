136f56c09ce86d9ebb0a5ba59c851996ccfdaceb
nidhikumari <kumari9936nidhi@gmail.com>
2022-09-21 17:11:05 +0530

    new project is added

---
+++ project-two/business-card/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-xl font-bold text-green-900'>Hello React + Tailwind With Vite</h1>
+    </div>
+  )
+}
+
+export default App
