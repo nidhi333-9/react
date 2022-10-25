24fbbdc19d853103df7031e5c417a6698423688d
nidhikumari <kumari9936nidhi@gmail.com>
2022-10-25 20:44:48 +0530

    async middleware

--- redux/redux-toolkit/index.js
+++ redux/redux-toolkit/index.js
@@ -1,21 +1,24 @@
 const store = require('./app/store')
 const { cakeActions } = require('./features/cake/cakeSlice')
 const { IcecreamActions } = require('./features/icecream/icecreamSlice')
+const { fetchUsers } = require('./features/user/userSlice')
 
 console.log('Intial State', store.getState())
 
 const unsubscribe = store.subscribe(() => {
-
+    console.log('Updated Store', store.getState())
 })
 
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.ordered())
-store.dispatch(cakeActions.restocked(3))
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.ordered())
+// store.dispatch(cakeActions.restocked(3))
 
 
-store.dispatch(IcecreamActions.ordered())
-store.dispatch(IcecreamActions.ordered())
-store.dispatch(IcecreamActions.restocked(2))
+// store.dispatch(IcecreamActions.ordered())
+// store.dispatch(IcecreamActions.ordered())
+// store.dispatch(IcecreamActions.restocked(2))
 
-unsubscribe();
+store.dispatch(fetchUsers())
+
+// unsubscribe();
